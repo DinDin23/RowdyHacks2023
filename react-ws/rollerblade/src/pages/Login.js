4b02fd898c8c7fd0c2590ff4204f8eecb6cb8235
Namish Kukreja <namishkukreja@Namishs-MacBook-Pro.local>
2023-03-26 02:52:07 -0500

    new branch

--- react-ws/rollerblade/src/pages/Login.js
+++ react-ws/rollerblade/src/pages/Login.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+<<<<<<< HEAD
 import "./Login.css"
+=======
+import './Login.css';
+>>>>>>> f80f1ce5 (login logout works)
 
 export default function Login(props) {
   const [ user, setUser ] = useState([]);
   const defaultProfile = useState({name: "", email: "", picture: null});
+<<<<<<< HEAD
   const [ profile, setProfile ] = useState(defaultProfile);
+=======
+  const [ profile, setProfile ] = defaultProfile;
+>>>>>>> f80f1ce5 (login logout works)
 
   const login = useGoogleLogin({
       onSuccess: (codeResponse) => setUser(codeResponse),
@@ -45,6 +53,7 @@ export default function Login(props) {
 
 
   return (
+<<<<<<< HEAD
     <div className="flexColumn" id='logintext'>
       <input placeholder="username" style={{width: 500}} value={profile.name} onChange={(e) => setProfile({...profile, name: e.target.value})}></input>
       <button onClick={seeLobbies}>See Lobbies</button>
@@ -57,6 +66,27 @@ export default function Login(props) {
             <p style={{marginBottom: "5em"}}>Email Address: {profile.email}</p>
             <button onClick={logOut}>Log out</button>
         </div>
+=======
+    <div id='logintext'>
+      <input style={{width: 500}} value={profile.name} onChange={(e) => setProfile({...profile, name: e.target.value})}></input>
+      <button onClick={joinLobby}>Join Lobby</button>
+      <h2>NeonBlader</h2>
+      <br style={{borderBottom: 200}}/> 
+      {profile.name && profile.name.length > 0 ? (
+          <div>
+              <img src={profile.picture} alt="user image" />
+              <p>Name: {profile.name}</p>
+              <p>Email Address: {profile.email}</p>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <button onClick={logOut}>Log out</button>
+          </div>
+>>>>>>> f80f1ce5 (login logout works)
       ) : (
           <button onClick={() => login()}>Sign in with Google 🚀 </button>
       )}
