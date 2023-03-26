9d247bb5fade07fb9a8cccdab5bb09ad6e50a48d
Dinesh Polisetty <dineshsp2003@gmail.com>
2023-03-26 10:35:40 -0500

    Spruce up post game

--- react-ws/rollerblade/src/pages/PostGame.js
+++ react-ws/rollerblade/src/pages/PostGame.js
@@ -1,6 +1,7 @@
 export default function PostGame(props) {
   return (
     <div className="flexColumn">
+      <h1>NeonBlader</h1>
       <h2>{props.winner} won!</h2>
       <button onClick={() => props.setPage("lobbyselector")}>Back</button>
     </div>
