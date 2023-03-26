f80f1ce5a6c76ff81ac3ac2d6112088e9854564f
Namish Kukreja <namishkukreja@Namishs-MacBook-Pro.local>
2023-03-26 01:13:45 -0500

    login logout works

--- react-ws/rollerblade/src/pages/Login.js
+++ react-ws/rollerblade/src/pages/Login.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import './Login.css';
 
 export default function Login(props) {
   const [ user, setUser ] = useState([]);
-  const [ profile, setProfile ] = useState({name: "", email: "", picture: null});
+  const defaultProfile = useState({name: "", email: "", picture: null});
+  const [ profile, setProfile ] = defaultProfile;
 
   const login = useGoogleLogin({
       onSuccess: (codeResponse) => setUser(codeResponse),
@@ -13,7 +15,7 @@ export default function Login(props) {
 
   const logOut = () => {
     googleLogout();
-    setProfile(null);
+    setProfile(defaultProfile);
   };
 
   useEffect(
@@ -43,17 +45,21 @@ export default function Login(props) {
 
 
   return (
-    <div>
+    <div id='logintext'>
       <input style={{width: 500}} value={profile.name} onChange={(e) => setProfile({...profile, name: e.target.value})}></input>
       <button onClick={joinLobby}>Join Lobby</button>
-      <h2>Google Login</h2>
-      <br />
-      <br />
-      {profile.name.length > 0 ? (
+      <h2>NeonBlader</h2>
+      <br style={{borderBottom: 200}}/> 
+      {profile.name && profile.name.length > 0 ? (
           <div>
               <img src={profile.picture} alt="user image" />
               <p>Name: {profile.name}</p>
               <p>Email Address: {profile.email}</p>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
               <br />
               <br />
               <button onClick={logOut}>Log out</button>
